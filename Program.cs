8b1919575ad6d6b7b12684db77570e06a3520dba
yungdeni <asertyx@gmail.com>
2020-11-12 11:19:10 +0100

    klart

--- Program.cs
+++ Program.cs
@@ -7,12 +7,65 @@ using System.Threading.Tasks;
 
 namespace Inlamning_2_ra_kod
 {
+    /* CLASS: Person
+    * PURPOSE: a person entry in the address list
+    */
     class Person
     {
-        public string namn, adress, telefon, email;
+        public string name, address, phone, email;
         public Person(string N, string A, string T, string E)
         {
-            namn = N; adress = A; telefon = T; email = E;
+            name = N; address = A; phone = T; email = E;
+        }
+        /* METHOD: Person (constructor)
+        * PURPOSE: Asks user for input if class initiliazed with no parameters
+        * PARAMETERS: None
+        */
+        public Person()
+        {
+            Console.WriteLine("Lägger till ny person");
+            Console.Write("  1. ange namn:    ");
+            this.name = Console.ReadLine();
+            Console.Write("  2. ange adress:  ");
+            this.address = Console.ReadLine();
+            Console.Write("  3. ange telefon: ");
+            this.phone = Console.ReadLine();
+            Console.Write("  4. ange email:   ");
+            this.email = Console.ReadLine();
+        }
+        /* METHOD: Person.Print
+        * PURPOSE: write a person entry to the address list file
+        * PARAMETERS: None
+        * RETURN VALUE: void
+        */
+        public void Print()
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", this.name, this.address, this.phone, this.email);
+        }
+        /* METHOD: Person.Set
+        * PURPOSE: Changes a specific field according to parameter values
+        * PARAMETERS: String inputField representing a field to be changed. String inputValue representing the value to change the field to
+        * RETURN VALUE: void
+        */
+        public void Set(string inputField, string inputValue)
+        {
+            switch (inputField)
+            {
+                case "namn":
+                    this.name = inputValue;
+                    break;
+                case "adress":
+                    this.address = inputValue;
+                    break;
+                case "telefon":
+                    this.phone = inputValue;
+                    break;
+                case "email":
+                    this.email = inputValue;
+                    break;
+                default:
+                    break;
+            }
         }
     }
     class Program
@@ -20,7 +73,101 @@ namespace Inlamning_2_ra_kod
         static void Main(string[] args)
         {
             List<Person> Dict = new List<Person>();
+            LoadList(Dict);
+            Console.WriteLine("Hej och välkommen till adresslistan");
+            Console.WriteLine("Skriv 'sluta' för att sluta!");
+            string command;
+            do
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                if (command == "sluta")
+                {
+                    Console.WriteLine("Hej då!");
+                }
+                else if (command == "ny")
+                {
 
+                    Dict.Add(new Person());
+                }
+                else if (command == "ta bort")
+                {
+                    RemovePerson(Dict);
+                }
+                else if (command == "visa")
+                {
+                    for (int i = 0; i < Dict.Count(); i++)
+                    {
+                        Dict[i].Print();
+                    }
+                }
+                else if (command == "ändra")
+                {
+                    ChangeDetails(Dict);
+                }
+                else
+                {
+                    Console.WriteLine("Okänt kommando: {0}", command);
+                }
+            } while (command != "sluta");
+        }
+        /* METHOD: ChangeDetails (static)
+        * PURPOSE: Searches a list for an object matching a name field and changes the fields in that object
+        * PARAMETERS: List<Person>
+        * RETURN VALUE: void
+        */
+        private static void ChangeDetails(List<Person> Dict)
+        {
+            Console.Write("Vem vill du ändra (ange namn): ");
+            string nameToChange = Console.ReadLine();
+            int found = -1;
+            for (int i = 0; i < Dict.Count(); i++)
+            {
+                if (Dict[i].name == nameToChange) found = i;
+            }
+            if (found == -1)
+            {
+                Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", nameToChange);
+            }
+            else
+            {
+                Console.Write("Vad vill du ändra (namn, adress, telefon eller email): ");
+                string fieldToChange = Console.ReadLine();
+                Console.Write("Vad vill du ändra {0} på {1} till: ", fieldToChange, nameToChange);
+                string newValue = Console.ReadLine();
+                Dict[found].Set(fieldToChange, newValue);
+            }
+        }
+        /* METHOD: RemovePerson (static)
+        * PURPOSE: Searches a list for an object matching a name field and removes the object from the list
+        * PARAMETERS: List<Person>
+        * RETURN VALUE: void
+        */
+        private static void RemovePerson(List<Person> Dict)
+        {
+            Console.Write("Vem vill du ta bort (ange namn): ");
+            string nameToRemove = Console.ReadLine();
+            int found = -1;
+            for (int i = 0; i < Dict.Count(); i++)
+            {
+                if (Dict[i].name == nameToRemove) found = i;
+            }
+            if (found == -1)
+            {
+                Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", nameToRemove);
+            }
+            else
+            {
+                Dict.RemoveAt(found);
+            }
+        }
+        /* METHOD: LoadList (static)
+        * PURPOSE: Reads a file and initializes objects into the list
+        * PARAMETERS: List<Person>
+        * RETURN VALUE: void
+        */
+        private static void LoadList(List<Person> Dict)
+        {
             Console.Write("Laddar adresslistan ... ");
             using (StreamReader fileStream = new StreamReader(@"..\..\address.lis"))
             {
@@ -35,91 +182,6 @@ namespace Inlamning_2_ra_kod
                 }
             }
             Console.WriteLine("klart!");
-
-            Console.WriteLine("Hej och välkommen till adresslistan");
-            Console.WriteLine("Skriv 'sluta' för att sluta!");
-            string command;
-            do
-            {
-                Console.Write("> ");
-                command = Console.ReadLine();
-                if (command == "sluta")
-                {
-                    Console.WriteLine("Hej då!");
-                }
-                else if (command == "ny")
-                {
-                    Console.WriteLine("Lägger till ny person");
-                    Console.Write("  1. ange namn:    ");
-                    string name = Console.ReadLine();
-                    Console.Write("  2. ange adress:  ");
-                    string adress = Console.ReadLine();
-                    Console.Write("  3. ange telefon: ");
-                    string telefon = Console.ReadLine();
-                    Console.Write("  4. ange email:   ");
-                    string email = Console.ReadLine();
-                    Dict.Add(new Person(name, adress, telefon, email));
-                }
-                else if (command == "ta bort")
-                {
-                    Console.Write("Vem vill du ta bort (ange namn): ");
-                    string villTaBort = Console.ReadLine();
-                    int found = -1;
-                    for (int i = 0; i < Dict.Count(); i++)
-                    {
-                        if (Dict[i].namn == villTaBort) found = i;
-                    }
-                    if (found == -1)
-                    {
-                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", villTaBort);
-                    }
-                    else
-                    {
-                        Dict.RemoveAt(found);
-                    }
-                }
-                else if (command == "visa")
-                {
-                    for (int i = 0; i < Dict.Count(); i++)
-                    {
-                        Person P = Dict[i];
-                        Console.WriteLine("{0}, {1}, {2}, {3}", P.namn, P.adress, P.telefon, P.email);
-                    }
-                }
-                else if (command == "ändra")
-                {
-                    Console.Write("Vem vill du ändra (ange namn): ");
-                    string villÄndra = Console.ReadLine();
-                    int found = -1;
-                    for (int i = 0; i < Dict.Count(); i++)
-                    {
-                        if (Dict[i].namn == villÄndra) found = i;
-                    }
-                    if (found == -1)
-                    {
-                        Console.WriteLine("Tyvärr: {0} fanns inte i telefonlistan", villÄndra);
-                    }
-                    else
-                    {
-                        Console.Write("Vad vill du ändra (namn, adress, telefon eller email): ");
-                        string fältAttÄndra = Console.ReadLine();
-                        Console.Write("Vad vill du ändra {0} på {1} till: ", fältAttÄndra, villÄndra);
-                        string nyttVärde = Console.ReadLine();
-                        switch (fältAttÄndra)
-                        {
-                            case "namn": Dict[found].namn = nyttVärde; break;
-                            case "adress": Dict[found].adress = nyttVärde; break;
-                            case "telefon": Dict[found].telefon = nyttVärde; break;
-                            case "email": Dict[found].email = nyttVärde; break;
-                            default: break;
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Okänt kommando: {0}", command);
-                }
-            } while (command != "sluta");
         }
     }
 }
